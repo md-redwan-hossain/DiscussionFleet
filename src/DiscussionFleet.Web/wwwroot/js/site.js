25f8f9cfd0bec86baf4b3a08762039487cc77f4a
md-redwan-hossain <redwan.ping@gmail.com>
2024-03-23 16:56:20 +0600

    added Registration.cshtml

--- src/DiscussionFleet.Web/wwwroot/js/site.js
+++ src/DiscussionFleet.Web/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function addTogglePasswordVisibilityHandler(buttonId, inputId) {
+    document.getElementById(buttonId).addEventListener('click', function (event) {
+        event.preventDefault();
+        const passwordInput = document.getElementById(inputId);
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+        } else {
+            passwordInput.type = "password";
+        }
+    });
+}
+
