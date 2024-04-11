c9eb94e15925627d16eb540e8f97acf13be8832e
md-redwan-hossain <redwan.ping@gmail.com>
2024-04-12 00:19:00 +0600

    added migrations

---
+++ src/DiscussionFleet.Infrastructure/Persistence/Migrations/20240411161700_updated multimedia image entity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscussionFleet.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedmultimediaimageentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Purpose",
+                table: "MultimediaImages",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_MultimediaImages_Purpose_Enum",
+                table: "MultimediaImages",
+                sql: "[Purpose] BETWEEN CAST(1 AS tinyint) AND CAST(3 AS tinyint)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_MultimediaImages_Purpose_Enum",
+                table: "MultimediaImages");
+
+            migrationBuilder.DropColumn(
+                name: "Purpose",
+                table: "MultimediaImages");
+        }
+    }
+}
---
+++ src/DiscussionFleet.Infrastructure/Persistence/Migrations/20240411170200_fixed member image fk cascade issue.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscussionFleet.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixedmemberimagefkcascadeissue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Members_MultimediaImages_ProfileImageId",
+                table: "Members");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Members_MultimediaImages_ProfileImageId",
+                table: "Members",
+                column: "ProfileImageId",
+                principalTable: "MultimediaImages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Members_MultimediaImages_ProfileImageId",
+                table: "Members");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Members_MultimediaImages_ProfileImageId",
+                table: "Members",
+                column: "ProfileImageId",
+                principalTable: "MultimediaImages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
